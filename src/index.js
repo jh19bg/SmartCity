959cbe1c7d4a4efe22eb26e6bf5c2e5bdd6bbbc4
Jon Hair 😺🛠 <johair@microsoft.com>
2022-11-18 01:01:56 -0500

    fix index

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-
 //import './App.css';
 import './assets/scss/style.scss';
 
